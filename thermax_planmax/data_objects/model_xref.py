ffba57e841df1dff8b98b586080879eaccd2931b
Sai Kiran <neo2603@gmail.com>
2024-08-02 00:35:33 +0530

    Update model_xref.py

--- thermax_planmax/data_objects/model_xref.py
+++ thermax_planmax/data_objects/model_xref.py
@@ -1,23 +1,23 @@
-from typing import TYPE_CHECKING
+# from typing import TYPE_CHECKING
 
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+# from sqlalchemy import Integer, String
+# from sqlalchemy.orm import Mapped, mapped_column
 
-from efficieno.ontology.base import ColumnMetadata, ObjectBase
+# from efficieno.ontology.base import ColumnMetadata, ObjectBase
 
-if TYPE_CHECKING:
-    pass
+# if TYPE_CHECKING:
+#     pass
 
 
-class ModelXref(ObjectBase):
-    __data_object_type__ = "data_object"
-    __tablename__ = "XXPLANMAX_MODEL_XREF"
-    __table_args__ = {"schema": "apps"}
+# class ModelXref(ObjectBase):
+#     __data_object_type__ = "data_object"
+#     __tablename__ = "XXPLANMAX_MODEL_XREF"
+#     __table_args__ = {"schema": "apps"}
 
-    xref_model_item: Mapped[str] = mapped_column('model_item', String, primary_key=True, info={"column_metadata": ColumnMetadata()})
-    xref_model_number: Mapped[str] = mapped_column('model_number', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
-    xref_product_planner: Mapped[str] = mapped_column('product_planner', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
-    xref_std_rsmh: Mapped[str] = mapped_column('std_rsmh', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
-    xref_prn_applicable: Mapped[str] = mapped_column('prn_applicable', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
-    xref_full_lead_time: Mapped[str] = mapped_column('full_lead_time', Integer, primary_key=False, info={"column_metadata": ColumnMetadata()})
-    xref_product_category: Mapped[str] = mapped_column('product_category', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
+#     xref_model_item: Mapped[str] = mapped_column('model_item', String, primary_key=True, info={"column_metadata": ColumnMetadata()})
+#     xref_model_number: Mapped[str] = mapped_column('model_number', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
+#     xref_product_planner: Mapped[str] = mapped_column('product_planner', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
+#     xref_std_rsmh: Mapped[str] = mapped_column('std_rsmh', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
+#     xref_prn_applicable: Mapped[str] = mapped_column('prn_applicable', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
+#     xref_full_lead_time: Mapped[str] = mapped_column('full_lead_time', Integer, primary_key=False, info={"column_metadata": ColumnMetadata()})
+#     xref_product_category: Mapped[str] = mapped_column('product_category', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
