980555f05b20386ed2de1d93ff17b1b762847aa0
Sai Kiran <neo2603@gmail.com>
2024-08-02 00:25:08 +0530

    Update model_xref.py

--- thermax_planmax/data_objects/model_xref.py
+++ thermax_planmax/data_objects/model_xref.py
@@ -14,10 +14,10 @@ class ModelXref(ObjectBase):
     __tablename__ = "XXPLANMAX_MODEL_XREF"
     __table_args__ = {"schema": "apps"}
 
-    model_item: Mapped[str] = mapped_column(String, primary_key=True, info={"column_metadata": ColumnMetadata()})
-    model_number: Mapped[str] = mapped_column(String, primary_key=False, info={"column_metadata": ColumnMetadata()})
-    product_planner: Mapped[str] = mapped_column(String, primary_key=False, info={"column_metadata": ColumnMetadata()})
-    std_rsmh: Mapped[str] = mapped_column(String, primary_key=False, info={"column_metadata": ColumnMetadata()})
-    prn_applicable: Mapped[str] = mapped_column(String, primary_key=False, info={"column_metadata": ColumnMetadata()})
-    full_lead_time: Mapped[str] = mapped_column(Integer, primary_key=False, info={"column_metadata": ColumnMetadata()})
-    product_category: Mapped[str] = mapped_column(String, primary_key=False, info={"column_metadata": ColumnMetadata()})
+    xref_model_item: Mapped[str] = mapped_column('model_item', String, primary_key=True, info={"column_metadata": ColumnMetadata()})
+    xref_model_number: Mapped[str] = mapped_column('model_number', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
+    xref_product_planner: Mapped[str] = mapped_column('product_planner', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
+    xref_std_rsmh: Mapped[str] = mapped_column('std_rsmh', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
+    xref_prn_applicable: Mapped[str] = mapped_column('prn_applicable', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
+    xref_full_lead_time: Mapped[str] = mapped_column('full_lead_time', Integer, primary_key=False, info={"column_metadata": ColumnMetadata()})
+    xref_product_category: Mapped[str] = mapped_column('product_category', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
